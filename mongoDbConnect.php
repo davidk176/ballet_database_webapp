928dcc240758df4518ae8451119f7904edb40654
David Karenke <david.karenke@live.com>
2021-08-11 18:40:58 +0200

    add mongodb

---
+++ mongoDbConnect.php
@@ -0,0 +1,12 @@
+<?php
+#echo phpinfo();
+$m = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+$query = new MongoDB\Driver\Query([]);
+
+// select a database
+$rows=$m->executeQuery("ballett.performance", $query);
+
+foreach($rows as $row){
+    echo $row->performanceName;
+}
+?>
